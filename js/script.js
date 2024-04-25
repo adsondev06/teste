3a0b5e6c88ce0a527820273014941aea81473ec0
DevNeres <149329464+adsondev06@users.noreply.github.com>
2024-04-25 00:07:10 -0300

    teste

--- js/script.js
+++ js/script.js
@@ -1,18 +1,162 @@
+// const barcodeResults = document.getElementById('barcode-results');
+// const codeCount = document.getElementById('count');
+// const successSound = document.getElementById('successSound');
+// const errorSound = document.getElementById('errorSound');
+// let detectedBarcodes = [];
+// let codeCounter = 0;
+// let errorDisplayed = false;
+
+// async function startBarcodeReader() {
+//     try {
+//         const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } });
+//         const video = document.getElementById('video');
+//         video.srcObject = stream;
+//         await video.play();
+//         setInterval(readBarcode, 3000); // Escaneia a cada 3 segundos
+//     } catch (error) {
+//         console.error('Erro ao iniciar a leitura do código de barras:', error);
+//         displayMessage('Erro ao iniciar a leitura do código de barras.', 'error');
+//     }
+// }
+
+// async function readBarcode() {
+//     try {
+//         const barcodeDetector = new BarcodeDetector();
+//         const barcodes = await barcodeDetector.detect(video);
+
+//         if (barcodes.length > 0) {
+//             barcodes.forEach(barcode => {
+//                 if (barcode.rawValue.length === 10) {
+//                     if (detectedBarcodes.includes(barcode.rawValue)) {
+//                         if (!errorDisplayed) {
+//                             displayMessage('Código de barras já lido.', 'error');
+//                             playErrorSound();
+//                             errorDisplayed = true;
+//                         }
+//                     } else {
+//                         if (errorDisplayed) {
+//                             clearError();
+//                         }
+//                         detectedBarcodes.push(barcode.rawValue);
+//                         const resultDiv = document.createElement('div');
+//                         const lastFourDigits = barcode.rawValue.slice(-4);
+//                         const formattedBarcode = barcode.rawValue.replace(lastFourDigits, `<span class="bold">${lastFourDigits}</span>`);
+//                         resultDiv.innerHTML = "Lido com sucesso: " + formattedBarcode;
+//                         resultDiv.classList.add('success');
+//                         barcodeResults.appendChild(resultDiv);
+//                         codeCount.textContent = detectedBarcodes.length;
+//                         playSuccessSound();
+//                         codeCounter++;
+//                         if (codeCounter === 3) {
+//                             barcodeResults.style.overflowY = 'scroll';
+//                         }
+//                     }
+//                 }
+//             });
+//         }
+//     } catch (error) {
+//         console.error('Erro ao detectar código de barras:', error);
+//         displayMessage('Erro ao detectar código de barras.', 'error');
+//     }
+// }
+
+// function formatBarcode(rawBarcode) {
+//     const lastFourDigits = rawBarcode.slice(-4);
+//     const formattedBarcode = rawBarcode.replace(lastFourDigits, `<span class="bold">${lastFourDigits}</span>`);
+//     return formattedBarcode;
+// }
+
+
+// function sendToWhatsApp() {
+//     const whatsappMessage = "Códigos lidos: *" + detectedBarcodes.length + "*\n\n" + detectedBarcodes.join("\n");
+//     window.open("https://api.whatsapp.com/send?text=" + encodeURIComponent(whatsappMessage), "_blank");
+// }
+
+// function playSuccessSound() {
+//     successSound.play();
+// }
+
+// function playErrorSound() {
+//     errorSound.play();
+// }
+
+// function displayMessage(message, type) {
+//     const messageDiv = document.createElement('div');
+//     messageDiv.textContent = message;
+//     messageDiv.classList.add(type);
+//     barcodeResults.appendChild(messageDiv);
+//     barcodeResults.scrollTop = barcodeResults.scrollHeight;
+// }
+
+// function clearError() {
+//     const errorDiv = document.querySelector('.error');
+//     if (errorDiv) {
+//         errorDiv.remove();
+//         errorDisplayed = false;
+//     }
+// }
+
+// function reloadPage() {
+//     window.location.reload();
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     startBarcodeReader();
+// });
+
+// const codeInput = document.getElementById('codeInput');
+
+// codeInput.addEventListener('keyup', function(event) {
+//     if (event.key === 'Enter') {
+//         const inputValue = this.value.trim();
+//         if (inputValue.length > 0 && !detectedBarcodes.includes(inputValue)) {
+//             detectedBarcodes.push(inputValue);
+//             const resultDiv = document.createElement('div');
+//             resultDiv.textContent = "Digitado manualmente: " + inputValue;
+//             resultDiv.classList.add('success');
+//             barcodeResults.appendChild(resultDiv);
+//             codeCount.textContent = detectedBarcodes.length;
+//             playSuccessSound();
+//             codeCounter++;
+//             if (codeCounter === 2) {
+//                 barcodeResults.style.overflowY = 'scroll';
+//             }
+//         } else {
+//             displayMessage('Valor inválido ou já inserido.', 'error');
+//             playErrorSound();
+//         }
+//         this.value = '';
+//     }
+// });
+
+
 const barcodeResults = document.getElementById('barcode-results');
 const codeCount = document.getElementById('count');
 const successSound = document.getElementById('successSound');
 const errorSound = document.getElementById('errorSound');
+const codeInput = document.getElementById('codeInput');
 let detectedBarcodes = [];
 let codeCounter = 0;
 let errorDisplayed = false;
+let video;
 
 async function startBarcodeReader() {
     try {
         const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } });
-        const video = document.getElementById('video');
+        const videoConstraints = { facingMode: "environment" };
+        if (navigator.mediaDevices.enumerateDevices) {
+            const devices = await navigator.mediaDevices.enumerateDevices();
+            const videoDevices = devices.filter(device => device.kind === 'videoinput');
+            const rearCamera = videoDevices.find(device => device.label.includes('back') || device.label.includes('rear'));
+            if (rearCamera) {
+                videoConstraints.deviceId = { exact: rearCamera.deviceId };
+            }
+        }
+        video = document.getElementById('video');
         video.srcObject = stream;
         await video.play();
-        setInterval(readBarcode, 3000); // Escaneia a cada 3 segundos
+        console.log("Câmera ativada com sucesso!");
+        setInterval(readBarcode, 3000);
     } catch (error) {
         console.error('Erro ao iniciar a leitura do código de barras:', error);
         displayMessage('Erro ao iniciar a leitura do código de barras.', 'error');
@@ -60,13 +204,6 @@ async function readBarcode() {
     }
 }
 
-function formatBarcode(rawBarcode) {
-    const lastFourDigits = rawBarcode.slice(-4);
-    const formattedBarcode = rawBarcode.replace(lastFourDigits, `<span class="bold">${lastFourDigits}</span>`);
-    return formattedBarcode;
-}
-
-
 function sendToWhatsApp() {
     const whatsappMessage = "Códigos lidos: *" + detectedBarcodes.length + "*\n\n" + detectedBarcodes.join("\n");
     window.open("https://api.whatsapp.com/send?text=" + encodeURIComponent(whatsappMessage), "_blank");
@@ -104,8 +241,6 @@ document.addEventListener('DOMContentLoaded', function() {
     startBarcodeReader();
 });
 
-const codeInput = document.getElementById('codeInput');
-
 codeInput.addEventListener('keyup', function(event) {
     if (event.key === 'Enter') {
         const inputValue = this.value.trim();
@@ -118,7 +253,7 @@ codeInput.addEventListener('keyup', function(event) {
             codeCount.textContent = detectedBarcodes.length;
             playSuccessSound();
             codeCounter++;
-            if (codeCounter === 2) {
+            if (codeCounter === 3) {
                 barcodeResults.style.overflowY = 'scroll';
             }
         } else {
